2e5a6c7bda23a8afdfd6f8b78119d2aa91fcf26f
Robininyourarea <mx.nattha963@gmail.com>
2026-03-02 13:41:55 +0700

    [EDIT] validate format date of birth

--- api/index.ts
+++ api/index.ts
@@ -31,9 +31,7 @@ const personalInfoSchema = z.object({
     firstName: z.string().min(1, 'First name is required'),
     lastName: z.string().min(1, 'Last name is required'),
     gender: z.string().min(1, 'Gender is required'),
-    dateOfBirth: z.string().refine((date) => !isNaN(Date.parse(date)), {
-        message: 'Invalid date format (e.g., 1990-12-31)'
-    }),
+    dateOfBirth: z.string().regex(/^\d{2}-\d{2}-\d{4}$/, 'Invalid date format (e.g., 02-03-2026)'),
     age: z.number().int('Age must be an integer').min(0, 'Age must be a positive number'),
     email: z.string().email('Invalid email format'),
     position: z.string().min(1, 'Position is required'),
